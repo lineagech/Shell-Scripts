95f3d5a41fcd86ae52f7a120c78ba2f445f7d3a1
Chia-Hao <lineagech@gmail.com>
2018-08-26 23:19:39 +0800

    update run-everywhere.sh

--- run-everywhere.sh
+++ run-everywhere.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Display the usage and exit.
+# A list of servers, one per line
+SERVER_LIST="/vagrant/servers"
+
+# Options for the ssh command
+SSH_OPTIONS='-o ConnectTimeout=2'
+
 function usage {
-  echo "Do not execute this script as root. Use the -s option instead."
-  echo "Usage: ./run-everywhere.sh [-nsv] [-f FILE] COMMAND"
-  echo "Executes COMMAND as a single command on every server."
-  echo "  -f FILE Use FILE for the list of servers. Default: /vargarnt/servers."
-  echo "  -n Dry run mode. Display the COMMAND that would have been executed and exit."
-  echo "  -s Execute the COMMAND using sudo on the remote server."
-  echo "  -v Verbose mode. Displays the server name before executing COMMAND."
+  # Display the usage and exit.
+  echo "Usage: ${0}[-nsv] [-f FILE] COMMAND" >&2
+  echo "Executes COMMAND as a single command on every server." >&2
+  echo "  -f FILE Use FILE for the list of servers. Default: /vargarnt/servers." >&2
+  echo "  -n Dry run mode. Display the COMMAND that would have been executed and exit." >&2
+  echo "  -s Execute the COMMAND using sudo on the remote server." >&2
+  echo "  -v Verbose mode. Displays the server name before executing COMMAND." >&2
   exit 1
 }
 
@@ -18,68 +23,70 @@ function log {
     echo "${1}"
   fi
 }
+
 # Make sure the script is not being executed with superuser privileges.
-if [[ ${UID} -eq 0 ]]
+if [[ "${UID}" -eq 0 ]]
 then
+  echo 'Do not execute this script as root. Use the -s option instead.' >&2 
   usage
 fi
 
 # Parse the options.
-FILE="/vagrant/servers"
 while getopts f:nsv OPTION
 do
   case ${OPTION} in
-    f)
-      FILE=${OPTARG}
-      ;;
-    n)
-      DRY_RUN='true'
-      ;;
-    s)
-      SUDO='sudo'
-      ;;
-    v)
-      VERBOSE='true'
-      ;;
-    ?)
-      usage
-      ;;
+    f) SERVER_LIST=${OPTARG} ;;
+    n) DRY_RUN='true' ;;
+    s) SUDO='sudo' ;;
+    v) VERBOSE='true' ;;
+    ?) usage ;;
   esac
 done
 
 # Remove the options while leaving the remaining arguments.
 shift "$(( OPTIND - 1 ))"
+
 # If the user doesn't supply at least one argument, give them help.
-if [[ -z ${1} ]]
+if [[ "${#}" -lt 1 ]]
 then
   usage
-else
-  COMMAND=${1}
 fi
+
 # Anything that remains on the command line is to be treated as a single command.
+COMMAND="${@}"
 
 # Make sure the SERVER_LIST file exists.
-if [[ ! -e ${FILE} ]]
+if [[ ! -e "${SERVER_LIST}" ]]
 then
-  echo "Cannot open server list file /path/to/nowhere."
+  echo "Cannot open server list file ${SERVER_LIST}." >&2
   exit 1
 fi
 
+# Expect the best, prepare for the worst.
+EXIT_STATUS='0'
+
 # Loop through the SERVER_LIST
-for SERVER in $(cat ${FILE})
+for SERVER in $(cat ${SERVER_LIST})
 do
+
+  SSH_COMMAND="ssh ${SSH_OPTIONS} ${SUDO} ${COMMAND}"
+
+  # If it is a dry run, do not execute anything. just echo it.
   if [[ ${DRY_RUN} = 'true' ]]
   then
     # If it's a dry run, don't execute anything, just echo it.
-    echo "ssh -o ConnectTimeout=2 "${SERVER}" "${COMMAND}""
+    echo "DRY_RUN: ${SSH_COMMAND}"
   else
-    ssh -o ConnectTimeout=2 "${SERVER}" "${SUDO}" "${COMMAND}"
+    ${SSH_COMMAND}
+    SSH_EXIT_STATUS="${?}"
+
     # Capture any non-zero exit status from the SSH_COMMAND and report to the user.
-    if [[ ${?} -ne 0 ]]
+    if [[ "${SSH_EXIT_STATUS}" -ne 0 ]]
     then
-      exit 1
+      EXIT_STATUS = "${SSH_EXIT_STATUS}"
+      echo "Execution on ${SERVER} failed." >&2
     fi
   fi
 done
 
-exit 0
+exit ${EXIT_STATUS}
